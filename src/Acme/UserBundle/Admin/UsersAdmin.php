c516f617e1c246d12636693d7cfb8da272d49ffa
«Myagkov» <a.myagkov@list.ru>
2014-07-09 20:59:25 +0400

    Admin panel finished

--- src/Acme/UserBundle/Admin/UsersAdmin.php
+++ src/Acme/UserBundle/Admin/UsersAdmin.php
@@ -7,7 +7,7 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-
+/*
 class UsersAdmin extends Admin
 {
     // Поля, отображаемые в формах create/edit
@@ -37,5 +37,89 @@ class UsersAdmin extends Admin
             ->add('slug')
             ->add('author')
         ;
+   
+   
+   }
+}
+*/
+
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use Knp\Menu\ItemInterface as MenuItemInterface;
+
+class UsersAdmin extends Admin
+{
+    /**
+     * Конфигурация отображения записи
+     *
+     * @param \Sonata\AdminBundle\Show\ShowMapper $showMapper
+     * @return void
+     */
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+                ->add('id', null, array('label' => 'Идентификатор'))
+                ->add('username', null, array('label' => 'Имя  юзера'))
+                ->add('email', null, array('label' => 'Емайл'));
     }
+
+    /**
+     * Конфигурация формы редактирования записи
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+                ->add('username', null, array('label' => 'Имя юзера'))
+                ->add('email', null, array('label' => 'Емайл'))
+                ->add('password', null, array('label' => 'Пароль'));
+    }
+
+    /**
+     * Конфигурация списка записей
+     *
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     * @return void
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+                ->addIdentifier('id')
+                ->addIdentifier('username', null, array('label' => 'Имя юзера'))
+                ->addIdentifier('email', null, array('label' => 'Емайл'));
+    }
+
+    /**
+     * Поля, по которым производится поиск в списке записей
+     *
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     * @return void
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+                ->add('username', null, array('label' => 'Имя юзера'))
+                ->add('email', null, array('label' => 'Емайл'));
+    }
+
+    /**
+     * Конфигурация левого меню при отображении и редатировании записи
+     *
+     * @param \Knp\Menu\ItemInterface $menu
+     * @param $action
+     * @param null|\Sonata\AdminBundle\Admin\Admin $childAdmin
+     *
+     * @return void
+     */
+     /*
+    protected function configureSideMenu(MenuItemInterface $menu, $action, Admin $childAdmin = null)
+    {
+        $menu->addChild(
+            $action == 'edit' ? 'Просмотр новости' : 'Редактирование юзера',
+            array('uri' => $this->generateUrl(
+                $action == 'edit' ? 'show' : 'edit', array('id' => $this->getRequest()->get('id'))))
+        );
+    }
+    */
 }
--- src/Acme/UserBundle/DependencyInjection/AcmeUserExtension.php
+++ src/Acme/UserBundle/DependencyInjection/AcmeUserExtension.php
@@ -1,22 +1,14 @@
 <?php
 
 namespace Acme\UserBundle\DependencyInjection;
-
+/*
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class AcmeUserExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
@@ -24,5 +16,25 @@ class AcmeUserExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+}
+*/
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+class AcmeUserExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+
+    public function getAlias()
+    {
+        return 'acme_user';
     }
 }
